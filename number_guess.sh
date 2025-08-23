4cd9d6d085b296991deeda7e2215ae74c3b09a41
sglavas <sglavas@m.ffzg.hr>
2025-08-23 12:47:22 -0400

    refactor: add existing code to function

--- number_guess.sh
+++ number_guess.sh
@@ -3,17 +3,23 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo -e "\n~~~ Number Guessing Game ~~~\n"
 
-echo "Enter your username:"
+GET_USER() {
+  echo "Enter your username:"
 
-read USERNAME
+  read USERNAME
 
-USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$USERNAME'")
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$USERNAME'")
 
-if [[ -z $USER_ID ]]
-then
-  INSERT_USERNAME_RESULT=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")
-  echo "Welcome, $USERNAME! It looks like this is your first time here."  
-else
-  echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
-fi
+  if [[ -z $USER_ID ]]
+  then
+    INSERT_USERNAME_RESULT=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")
+    echo "Welcome, $USERNAME! It looks like this is your first time here."  
+  else
+    echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
+  fi
+}
+
+
+GET_USER
+
 
