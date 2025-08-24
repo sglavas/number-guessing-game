c4621ac560b4aa991a62ccb0f0517e4b22aa7c4b
sglavas <sglavas@m.ffzg.hr>
2025-08-24 06:51:37 -0400

    fix: drop NOT NULL constraint from games_played and best_game

--- number_guess.sql
+++ number_guess.sql
@@ -50,8 +50,8 @@ SET default_table_access_method = heap;
 CREATE TABLE public.users (
     user_id integer NOT NULL,
     name character varying(22) NOT NULL,
-    games_played integer NOT NULL,
-    best_game integer NOT NULL
+    games_played integer,
+    best_game integer
 );
 
 
@@ -90,13 +90,15 @@ ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.u
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.users VALUES (2, 'test_user', NULL, NULL);
+INSERT INTO public.users VALUES (3, 'Pedro', NULL, NULL);
 
 
 --
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_user_id_seq', 3, true);
 
 
 --
