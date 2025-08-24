ab7ade2f95df87a8ea3f4c0c7a20b2cb7464ac51
sglavas <sglavas@m.ffzg.hr>
2025-08-24 04:46:21 -0400

    feat: add conditional checks to PLAY_GAME
    Make sure that PLAY_GAME checks for argument.
    Add conditional check to check if USER_GUESS is greater than or less than RANDOM_NUMBER.
    Move RANDOM_NUBMER outside of the function.

--- number_guess.sh
+++ number_guess.sh
@@ -3,6 +3,11 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo -e "\n~~~ Number Guessing Game ~~~\n"
 
+MIN=1
+MAX=1000
+
+RANDOM_NUMBER=$(($RANDOM%($MAX-$MIN+1)+$MIN))
+
 GET_USER() {
   echo "Enter your username:"
 
@@ -20,16 +25,28 @@ GET_USER() {
 }
 
 PLAY_GAME() {
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+
   echo -e "\nGuess the secret number between 1 and 1000:"
 
   read USER_GUESS
 
-  MIN=1
-  MAX=1000
-  
-  RANDOM_NUMBER=$(($RANDOM%($MAX-$MIN+1)+$MIN))
 
-
+  if [[ $USER_GUESS -gt $RANDOM_NUMBER ]]
+  then
+    echo "The random number is $RANDOM_NUMBER"
+    PLAY_GAME "It's lower than that, guess again:"
+  elif [[ $USER_GUESS -lt $RANDOM_NUMBER ]]
+  then
+    echo "The random number is $RANDOM_NUMBER"
+    PLAY_GAME "It's higher than that, guess again:"
+  else
+    echo "The random number is $RANDOM_NUMBER"
+    echo -e "\nYou guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"
+  fi
 }
 
 GET_USER
